fbe14de6061525679bb1dfb8d8680d7ec0185d07
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-08-23 15:29:03 +0530

    added responsiveness in search page

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import "./search.css";
 import SearchUsers from "../Homepage/SearchUsers/SearchUsers";
+import menuImg from "../../Assets/menuIcon.svg";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
+  const navigate = useNavigate();
+
+  const showMenu = () => {
+    alert("i");
+    // navigate("/sidebar");
+  };
+
   return (
     <>
       <div className="search-page">
         <Sidebar />
+
         <div className="search-sidebar">
+          <img
+            src={menuImg}
+            alt="menu"
+            className="menu-icon-search"
+            id="menu2"
+            onClick={showMenu}
+          />
           <SearchUsers />
         </div>
       </div>
